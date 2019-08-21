1d95562662f39e525fad0e5cd7351f5cb9990751
Matt McNeive <mattmcneive@Matts-MacBook-Pro.local>
2019-08-21 10:23:17 -0500

    Small changes

--- script.py
+++ script.py
@@ -11,14 +11,15 @@ def parse_amazon(url, user_price):
     soup = BeautifulSoup(page.content, 'html5lib')
 
     price = soup.find('span', attrs = {'id': 'priceblock_ourprice'}).get_text()
-    price_updated = int(price[1:3])
-    print(price_updated)
+    price_updated = float(price[1:])
     if price_updated < user_price:
-        send_email()
+        send_email(price_updated)
+    else:
+        print('The current price is ${0}'.format(price_updated))
 
 
 
-def send_email():
+def send_email(price):
     server = smtplib.SMTP('smtp.gmail.com', 587)
     server.ehlo()
     server.starttls()
@@ -26,11 +27,11 @@ def send_email():
 
     server.login('matt.mcneive@gmail.com', 'fuczqthhokialmwl')
     subject = 'Price Drop'
-    body = 'Amazon price fell    https://www.amazon.com/dp/B01G6IOEF6/ref=twister_B07GR6XHJ8?_encoding=UTF8&psc=1'
+    body = 'Amazon price fell to ${0}   https://www.amazon.com/dp/B01G6IOEF6/ref=twister_B07GR6XHJ8?_encoding=UTF8&psc=1'.format(price)
     msg = f"Subject: {subject}\n\n{body}"
-    server.sendmail('matt.mcneive@gmail.com', 'mmcneive@luc.edu', msg)
+    server.sendmail('matt.mcneive@gmail.com', 'matt.mcneive@gmail.com', msg)
     print('Email Sent')
     server.quit()
 
 
-parse_amazon('https://www.amazon.com/dp/B01G6IOEF6/ref=twister_B07GR6XHJ8?_encoding=UTF8&psc=1', 25)
+parse_amazon('https://www.amazon.com/dp/B01G6IOEF6/ref=twister_B07GR6XHJ8?_encoding=UTF8&psc=1', 26.00)
